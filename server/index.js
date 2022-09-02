633e11130e4948ac5e45287b22271722e828e002
AMAN RAJ <amanrajgp@gmail.com>
2022-09-02 18:20:50 +0530

    add insertion and show function in app

---
+++ server/index.js
@@ -0,0 +1,41 @@
+const express=require("express");
+const app=express();
+const mysql=require("mysql");
+const cors=require("cors");
+
+app.use(cors());
+app.use(express.json());
+
+const db=mysql.createConnection({
+    user: "root",
+    host: "localhost",
+    password: "password",
+    database:"Bank_DBMS_app"
+});
+
+app.post("/create",(req,res)=>{
+    const name=req.body.name;
+    const age=req.body.age;
+    const country =req.body.country;
+    const position=req.body.position;
+    const salary=req.body.salary;
+
+    db.query(
+        "INSERT INTO customers_info (name,age,country,position,salary) VALUES (?,?,?,?,?)",
+        [name,age,country,position,salary],
+        (err,result)=>{
+            if (err) {
+                console.log(err);
+            }else {
+                res.send("values inserted");
+            }
+        }
+    );
+});
+
+
+
+
+app.listen(3001,()=>{
+    console.log("server is running on port 3001");
+});
